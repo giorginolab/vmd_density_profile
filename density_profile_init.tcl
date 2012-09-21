04ab44555b491b89e3e119a615e99805c910dcb5
Toni <toni.giorgino@isib.cnr.it>
2012-09-21 12:09:20 +0200

    Adding from tabularasa

---
+++ density_profile_init.tcl
@@ -0,0 +1,23 @@
+# VMD Density Profile tool - a GUI to compute various types of density
+# profiles
+#
+#     Copyright (C) 2011  Universitat Pompeu Fabra
+#     Author              Toni Giorgino  (toni.giorgino@gmail.com)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id: density_profile_vmd.tcl 688 2011-02-09 13:25:32Z toni $ 
+
+
+package require density_profile
