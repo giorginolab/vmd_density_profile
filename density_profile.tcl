1f2347e16834712ef4c8f200a24dd8065da2e6a8
Toni Giorgino <toni.giorgino@isib.cnr.it>
2012-12-13 16:18:44 +0100

    help

--- density_profile.tcl
+++ density_profile.tcl
@@ -1,12 +1,16 @@
+# Core functions for computing density profiles.
 
 
 
 package provide density_profile 0.3
 
 # Declare the namespace for this dialog
-namespace eval density_profile {
+namespace eval ::density_profile:: {
+
+
+    # Variables matching command line options
     variable target atoms
-    variable selection lipid
+    variable selection all
     variable axis z
     variable resolution 1
     variable ansource type
@@ -14,27 +18,42 @@ namespace eval density_profile {
     variable frame_from now
     variable frame_to now
     variable frame_step 1
-#     if $density_profile::in_vmd {
-# 	variable frame_from [molinfo top get frame]
-# 	variable frame_to [molinfo top get frame]
-# 	variable frame_step 1
-#     }
-    puts {$Id: density_profile.tcl 854 2011-05-24 17:11:45Z toni $}
+    variable average 0
+
+    # Atom numbers
+    variable name_to_Z {H 1  C 6  N 7  O 8  F 9  P 15  S 16}
+
+    variable args_list {target selection axis resolution ansource partial_charges \
+			    frame_from frame_to frame_step average}
 }
 
 
 
+proc density_profile { args } { return [eval ::density_profile::density_profile $args] }
+
+
+proc ::density_profile::density_profile_usage { } {
+    variable args_list
+    puts "Usage: density_profile <args>"
+    puts "Args (with defaults):"
+    foreach a $args_list {
+	puts "   -$a [set ${a}]"
+    }
+}
+
 
 
 # Get Z number from atomselection CORE
-proc density_profile::getZ {as} {
+proc ::density_profile::getZ {as} {
     variable ansource
+    variable name_to_Z
+
     set nlist [$as get $ansource]
     set res {}
     set unk {}
     set warns 0
 
-    array set ztable {H 1  C 6  N 7  O 8  F 9  P 15  S 16}
+    array set ztable $name_to_Z
     foreach n $nlist {
 	set el [string range $n 0 0]
 	if {[info exists ztable($el)]} {
@@ -56,7 +75,7 @@ proc density_profile::getZ {as} {
 
 # Sanity check on PBC. Return -1 if error occurred but densities can
 # be still computed, -2 if not, else return transversal PBC area CORE
-proc density_profile::assertpbc { } {
+proc ::density_profile::assertpbc { } {
     variable axis
     lassign [molinfo top get {a b c alpha beta gamma}] a b c alpha beta gamma
 
@@ -76,9 +95,10 @@ proc density_profile::assertpbc { } {
 }
 
 
-# return the range over the 1st and 2nd dimension of a 2d array
+# return the range over the 1st and 2nd dimension of a pseudo-2d array
+# e.g. {2,3 5,4 2,4} -> {2 5 3 4}
 # CORE
-proc density_profile::get_keys_range {kk} {    
+proc ::density_profile::get_keys_range {kk} {    
     foreach k $kk {
 	lappend flist [lindex [split $k ,] 0]
 	lappend xlist [lindex [split $k ,] 1]
@@ -87,9 +107,9 @@ proc density_profile::get_keys_range {kk} {
     set xlist [lsort -uniq -integer $xlist]
 
     set fmin [lindex $flist 0]
-    set fmax [lindex [lreverse $flist] 0]
+    set fmax [lindex $flist end]
     set xmin [lindex $xlist 0]
-    set xmax [lindex [lreverse $xlist] 0]
+    set xmax [lindex $xlist end]
     
     return [list $fmin $fmax $xmin $xmax]
 }
@@ -98,7 +118,7 @@ proc density_profile::get_keys_range {kk} {
 # fill histogram keys so that there is one integer bin per each value
 # between mi and max
 # CORE
-proc density_profile::fill_keys arr {
+proc ::density_profile::fill_keys arr {
     upvar $arr inp
     lassign [get_keys_range [array names inp]] fmin fmax xmin xmax
     puts "Filling frames $fmin..$fmax, bins $xmin..$xmax"
@@ -117,7 +137,7 @@ proc density_profile::fill_keys arr {
 
 # Fix frame range to current frame if not specified
 # CORE
-proc density_profile::fix_frame_range {} {
+proc ::density_profile::fix_frame_range {} {
     variable frame_from;  variable frame_to; variable frame_step
     set fr [ list $frame_from $frame_to $frame_step ]
     if { $frame_from=="now" } { lset fr 0 [molinfo top get frame] }
@@ -129,7 +149,7 @@ proc density_profile::fix_frame_range {} {
 # Return the values of the selected property, as a list of one value
 # per selected atom. These will not change per-frame.
 # CORE
-proc density_profile::get_values {} {
+proc ::density_profile::get_values {} {
     variable selection
     variable target
     variable partial_charges
@@ -157,7 +177,7 @@ proc density_profile::get_values {} {
 }
 
 
-proc density_profile::compute {} {
+proc ::density_profile::compute {} {
 
     # MOVE AWAY THIS CHECK
     set area [assertpbc]
@@ -172,7 +192,7 @@ proc density_profile::compute {} {
     set tval [get_values]
     set tval [vecscale [expr 1./$area/$resolution] $tval]
 
-    # Frame range
+    # Frame range - REMOVE
     lassign [fix_frame_range] ffrom fto fstep
 
 
